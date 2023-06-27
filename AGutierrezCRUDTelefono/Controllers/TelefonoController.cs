491a9ec2500949f756ad71777d0b76e1f6cdde5c
alejandragutierrezglez <alejandragutierrezglez2023@gmail.com>
2023-06-27 14:38:03 -0600

    Agregar archivos de proyecto.

---
+++ AGutierrezCRUDTelefono/Controllers/TelefonoController.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AGutierrezCRUDTelefono.Controllers
+{
+    public class TelefonoController : Controller
+    {
+        // GET: Telefono
+        public ActionResult GetAll()
+        {
+            ML.Telefono telefono = new ML.Telefono();
+            ML.Result result = BL.Telefono.GetAll();
+
+            if (result.Correct)
+            {
+                telefono.Telefonos = result.Objects;
+                return View(telefono);
+            }
+            else
+            {
+                return View(telefono);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult Form(int? IdTelefono)
+        {
+            ML.Telefono telefono = new ML.Telefono();
+            ML.Result resultTelefono = BL.Telefono.GetAll();
+            if (resultTelefono.Correct)
+            {
+                telefono.Telefonos = resultTelefono.Objects;
+            }
+
+            if (IdTelefono == null)
+            {
+                return View(telefono);
+            }
+            else
+            {
+                ML.Result result = BL.Telefono.GetById(IdTelefono.Value);
+
+                if (result.Correct)
+                {
+
+                    telefono = (ML.Telefono)result.Object;
+                    telefono.Telefonos = resultTelefono.Objects;
+                    return View(telefono);
+                }
+                else
+                {
+                    ViewBag.Message = "Ocurrio al consultar la información del telefono";
+                    return View("Modal");
+                }
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Form(ML.Telefono telefono)
+        {
+            ML.Result result = new ML.Result();
+
+            if (telefono.IdTelefono != null)
+            {
+                result = BL.Telefono.Update(telefono);
+                ViewBag.Message = "Se ha modificado el registro";
+            }
+            else
+            {
+                result = BL.Telefono.Add(telefono);
+                ViewBag.Message = "Se ha agregado el registro";
+            }
+            if (result.Correct)
+            {
+                return PartialView("Modal");
+            }
+            else
+            {
+                return PartialView("Modal");
+            }
+        }
+        public ActionResult Delete(int IdTelefono)
+        {
+            ML.Result result = BL.Telefono.Delete(IdTelefono);
+            if (result.Correct)
+            {
+                ViewBag.Message = "Se ha eliminado el registro";
+                return PartialView("Modal");
+            }
+            else
+            {
+                ViewBag.Message = "No se ha podido eliminar el registro seleccionado" + result.ErrorMessage;
+                return PartialView("Modal");
+            }
+
+        }
+
+    }
+
+}
---
+++ BL/Telefono.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class Telefono
+    {
+        public static ML.Result GetAll()
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.AGutierrezCRUDTelefonoEntities context = new DL.AGutierrezCRUDTelefonoEntities())
+                {
+                    var query = context.TelefonoGetAll().ToList();
+                    if (query != null)
+                    {
+                        result.Objects = new List<object>();
+
+                        foreach (var item in query)
+                        {
+                            ML.Telefono telefono = new ML.Telefono();
+                            telefono.IdTelefono = item.IdTelefono;
+                            telefono.Marca = item.Marca;
+                            telefono.Modelo = item.Modelo;
+                            telefono.NumSerie = item.NumSerie;
+
+                            result.Objects.Add(telefono);
+                            result.Correct = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = Ex.Message;
+                result.Ex = Ex;
+            }
+            return result;
+        }
+        public static ML.Result GetById(int IdTelefono)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.AGutierrezCRUDTelefonoEntities context = new DL.AGutierrezCRUDTelefonoEntities())
+                {
+                    var query = context.TelefonoGetById(IdTelefono).FirstOrDefault();
+                    if (query != null)
+                    {
+                        ML.Telefono telefono = new ML.Telefono();
+                        telefono.IdTelefono = query.IdTelefono;
+                        telefono.Marca = query.Marca;
+                        telefono.Modelo = query.Modelo;
+                        telefono.NumSerie = query.NumSerie;
+
+                        result.Object = telefono;
+                        result.Correct = true;
+
+                    }
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = Ex.Message;
+                result.Ex = Ex;
+            }
+            return result;
+        }
+        public static ML.Result Add(ML.Telefono telefono)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.AGutierrezCRUDTelefonoEntities context = new DL.AGutierrezCRUDTelefonoEntities())
+                {
+                    var query = context.TelefonoAdd(telefono.Marca, telefono.Modelo, telefono.NumSerie);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = Ex.Message;
+                result.Ex = Ex;
+            }
+            return result;
+        }
+        public static ML.Result Update(ML.Telefono telefono)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.AGutierrezCRUDTelefonoEntities context = new DL.AGutierrezCRUDTelefonoEntities())
+                {
+                    var query = context.TelefonoUpdate(telefono.IdTelefono, telefono.Marca, telefono.Modelo, telefono.NumSerie);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = Ex.Message;
+                result.Ex = Ex;
+            }
+            return result;
+        }
+        public static ML.Result Delete(int IdTelefono)
+        {
+            ML.Result result = new ML.Result();
+            try
+            {
+                using (DL.AGutierrezCRUDTelefonoEntities context = new DL.AGutierrezCRUDTelefonoEntities())
+                {
+                    var query = context.TelefonoDelete(IdTelefono);
+                    if (query > 0)
+                    {
+                        result.Correct = true;
+                    }
+                    else
+                    {
+
+                        result.Correct = false;
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                result.Correct = false;
+                result.ErrorMessage = Ex.Message;
+                result.Ex = Ex;
+            }
+            return result;
+        }
+
+    }
+}
